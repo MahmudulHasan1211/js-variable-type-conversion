d3d720f750f52715a6bbb73a1ceb37c867992588
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-08-31 23:55:01 +0600

    first commit

---
+++ variable type change.js
@@ -0,0 +1,17 @@
+var applePrice = 220;
+var orangePrice ='190' //string
+orangePrice = parseInt('190') //convert string into number
+
+console.log(typeof applePrice)
+console.log(typeof orangePrice)
+console.log(orangePrice+applePrice)
+
+
+var tk_rate = 0.1;
+var usd =parseFloat ('0.2'); //change into float number
+
+
+console.log(tk_rate)
+console.log(usd)
+var total = tk_rate + usd;
+console.log(total.toFixed(2)) //tofixed fixed the valu of number after point
